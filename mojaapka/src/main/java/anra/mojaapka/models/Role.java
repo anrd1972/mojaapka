53727acd29c765543576b8eaf591b93ad4192e39
Andrzej <anrd1972@gmail.com>
2017-11-14 12:54:16 +0100

    Added model classes

---
+++ mojaapka/src/main/java/anra/mojaapka/models/Role.java
@@ -0,0 +1,52 @@
+package anra.mojaapka.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="role")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="role_id")
+	private int id;
+	
+	@Column(name="role")
+	private String role;
+	
+	//--- GETTERS AND SETTERS
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	/**
+	 * @return the role
+	 */
+	public String getRole() {
+		return role;
+	}
+	
+	/**
+	 * @param role the role to set
+	 */
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+}
